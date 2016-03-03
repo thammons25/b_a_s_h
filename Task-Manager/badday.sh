d6f2d7456ec382bac24939360db6932d06553729
Thomas Hammons <thomas.hammons_jr@uconn.edu>
2016-03-03 16:29:36 -0500

    Create badday.sh
    fun program for a rough day

---
+++ Task-Manager/badday.sh
@@ -0,0 +1,11 @@
+echo -n "Enter your wonderful name, you wonderful person:"
+read userName
+echo -e "\n"
+echo "Dear," $userName
+echo -e "\tToday is going to be a great day."
+echo -e "\tYou got this!"
+echo -e "\tYou're an amazing person and you can do anything!"
+echo -e "\tYou got this!!!!"
+echo -e "\nLove,"
+echo -e "Your computer, and the whole rest of the world."
+echo -e "\n"
